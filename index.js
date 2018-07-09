9c282b77d6db5b681ec1c0adfb7429ac6785814f
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-09 21:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments){
   var strings = [];
   for (var i = 0; i< musicians.length; i++) {
-    strings.push('${musicians[i]} plays ${instruments[i]}');
+    strings.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return strings;
 }
