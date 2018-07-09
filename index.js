6140d14efa3db5d7f0589ceeff9637f0056bfb36
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-09 21:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
-  var strings = {};
-  for (i = 0, i< musicians.length, i++)
-  
+  var strings = [];
+  for (i = 0; i< musicians.length; i++) {
+    strings.push("${musicians[i]} plays ${instruments[i]}");
+  }
+  return strings;
 }
