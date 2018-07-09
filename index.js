bd88d6877c5e1cdfa5da62ae2c703d220be7983c
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-09 21:48:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function johnLennonFacts(facts) {
   }
   return facts;
 }
+
+function iLoveTheBeatles(n) {
+  var lovely = [];
+  do {
+    lovely.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return lovely;
+}
